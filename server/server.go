780d930127c1ce6e3ee8d3697f41b5c725c9371f
lifintsefv <157622370+Llifintsefv@users.noreply.github.com>
2024-11-25 20:27:36 +0300

    add server

--- server/server.go
+++ server/server.go
@@ -1,24 +1,69 @@
-package proxy
+package server
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net"
 	"net/http"
-	"net/url"
+	"os"
+	"strconv"
 	"time"
 )
 
 const (
-	defaultTargetUrl = "http://localhost:8081"
-	defailServerPort = "8080"
+	defaultShutdownTimeout = 5 * time.Second
 )
 
-var (
-	targetURL *url.URL
-	serverPort string
-	backoffSchedule = []time.Duration{
-		1* time.Second,
-		3 * time.Second,
-		10 * time.Second,
+type Server struct {
+	srv *http.Server
+	port string
+	shutdownTimeout time.Duration
+}
+
+func NewServer(handler http.Handler,port string) *Server{
+	address := net.JoinHostPort("0.0.0.0",port)
+	timeoutString := os.Getenv("SERVER_SHUTDOWN_TIMEOUT")
+	timeout := defaultShutdownTimeout
+	if timeoutString != "" {
+		timeoutInt,err := strconv.Atoi(timeoutString)
+		if err != nil {
+			timeout = time.Duration(timeoutInt) * time.Second
+		} else {
+			log.Printf("Invalid SERVER_SHUTDOWN_TIMEOUT: %v, default: %v",err,defaultShutdownTimeout)
+		}
+	} 
+	src := &http.Server{
+		Addr: address,
+		Handler: handler,
 	}
-	client = http.Client{Timeout: 30 * time.Second}
-)
 
+	return &Server{
+		srv: src,
+		port: port,
+		shutdownTimeout: timeout,
+	}
+
+}
+
+func (s *Server) Run(ctx context.Context) error{
+	log.Printf("Starting server on port %s",s.port)
+	go func() {
+		err := s.srv.ListenAndServe()
+		if err != nil && !errors.Is(err,http.ErrServerClosed) {
+			log.Printf("Liste and serve error := %v ",err)
+		}
+	}()
+	<-ctx.Done()
+
+	log.Println("Shutting down server")
+	downCtx,cancel := context.WithTimeout(context.Background(),s.shutdownTimeout)
+	defer cancel()
+
+	if err := s.srv.Shutdown(downCtx); err != nil {
+		return fmt.Errorf("shutdown %w",err)
+	}
+	log.Println("Server gracefully stopped")
+	return nil
+}
